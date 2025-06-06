1519171c89361e67b50a250dd3ace13575353b5b
MilanaINN <milana.charnyshova@innowise.com>
2025-06-06 18:52:06 +0300

    Create Geo.java

---
+++ AI-Backend/src/main/java/com/jsonplaceholder/api/model/Geo.java
@@ -0,0 +1,15 @@
+package com.jsonplaceholder.api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+@Data
+@Embeddable
+public class Geo {
+    @Column(name = "geo_lat")
+    private String lat;
+    
+    @Column(name = "geo_lng")
+    private String lng;
+} 
