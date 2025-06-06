203eb4eca62332584cd78095cd50acfbfdc59e71
MilanaINN <milana.charnyshova@innowise.com>
2025-06-06 19:30:53 +0300

    Create App.tsx

---
+++ AI-Frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import UserTable from './components/UserTable/UserTable';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <UserTable />
+    </div>
+  );
+}
+
+export default App; 
